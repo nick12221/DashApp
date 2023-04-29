d5c469c44291bf6f7abf1d80b7c89a12bdda5d12
nick12221 <50280244+nick12221@users.noreply.github.com>
2023-04-29 14:32:21 -0700

    continuing adding

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import dash
 from dash import Dash, html, dcc, no_update, dash_table as dt
 from dash.dependecies import Input, Output, State
 import dash_bootstrap_components as dbc
+
+
+#Initialize app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div()
