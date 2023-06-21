90caad793e554a81dde4c0d15a314feb58b029da
nick12221 <50280244+nick12221@users.noreply.github.com>
2023-06-20 20:26:04 -0700

    class app

--- TabTwoComponents/tab_two_components.py
+++ TabTwoComponents/tab_two_components.py
@@ -5,89 +5,18 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 
-instructions_pulling_movies_form = dbc.Form(id='instructions-for-cluster-tab-id',
-                                children=[
-                                    dcc.Markdown('''
-                                    * Configure K Means Clustering algorithm using the variable selection and number of clusters options below. Click the "Run" button to get results.
+predictions__choies = dcc.RadioItems(['Use Imported Data', 'Build Own Prediction'], 'Use Imported Data')
 
-                                    * Analyze the types of clusters in the data table and use the charts to evaluate the algorithm.
-                                    ''')
-                                ])
+instructions_running_predictions = dbc.Form(id='instructions-for-model-id',
+                                            children=[
+                                                dcc.Markdown('''
+                                                * Please upload a CSV or excel with one column for the movies you want to pull from the OMDB API. App only allows for pulling 1,000 movies per day. Please click the "import" button to pull the data into the app.
 
-cluster_options_dropdown = dcc.Dropdown(id = 'cluster-options-dropdown-id',
-                            placeholder="Select variables for K Means Clustering")
+                                                * The second tab performs segmentation using K Means Clustering.
 
-num_clusters_input = dcc.Input(id='num-cluster-input-id',
-                            type='number',
-                            placeholder='Please enter number of clusters',
-                            min=0)
+                                                * The third tab is responsible for statistical learning, where the user can build their own linear regression model.
 
+                                                * The fourth tab you can upload your own description for a movie and it will make a prediction for what genre(s) it could belong to.
+                                            ''')
+                                            ])
 
-run_cluster_algorithm = html.Button(run_cluster_message,
-                                        id='run-cluster-btn-id',
-                                        n_clicks=0)
-
-export_clusters_btn = html.Button(export_cluster_table_message,
-                                        id='export-cluster-table-btn-id',
-                                        n_clicks=0)
-
-
-configure_cluster_algorithm = html.Div(id='configure-div-id', children=[
-                                dbc.Row([
-                                    html.Div(id='variable-dropdown-div-id', children=[
-                                        dbc.Col([
-                                            cluster_options_dropdown
-                                        ])
-                                    ]),
-                                    html.Div(id='num_cluster_input-div-id', children=[
-                                        dbc.Col([
-                                            num_clusters_input
-                                        ])
-                                    ]),
-                                    html.Div(id='run-cluster-div-id', children=[
-                                        dbc.Col([
-                                            run_cluster_algorithm
-                                        ])
-                                    ]),
-                                    html.Div(id='export-div-id', children=[
-                                        dbc.Col([
-                                            export_clusters_btn
-                                        ])
-                                    ])
-                                ])
-                            ])
-
-cluster_result_table = dt.DataTable(id='cluster-table-id',
-                                        filter_action='native',
-                                        sort_action='native',
-                                        style_header={'backgroundColor':'#ACC0C4',
-                                                    'fontWeight':'bold'}
-                                        )
-
-elbow_graph = dcc.Graph(id='elbow-graph-id')
-
-silhoutte_graph = dcc.Graph(id='silhouette-graph-id')
-
-results_section = html.Div(id='all-result-div-id',children = [
-    dbc.Row([
-        html.Div(id='result-table-div-id', children=[
-            dbc.Col([
-                cluster_result_table
-            ])
-        ]),
-        html.Div(id='evaluation-div-id',children=[
-                dbc.Col([
-                    html.Div(id='elbow-div-id', children=[
-                        dbc.Row([
-                            elbow_graph
-                        ])
-                    ]),
-                    html.Div(id='silhouette-div-id', children=[
-                        dbc.Row([
-                            silhoutte_graph
-                        ])
-                    ])
-                ])
-            ])
-        ])
-    ])
--- app.py
+++ app.py
@@ -5,43 +5,59 @@ from threading import Timer
 from dash import Dash, html, dcc, no_update, dash_table as dt
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from CallbackFunctions.file_import_export import *
-from CallbackFunctions.movie_api import *
+from CallbackFunctions.movie_api import MovieRequests
 from TabOneComponents.tab_one_components import *
 from TabTwoComponents.tab_two_components import *
 
-#initialize class objects
-FileImportExport = FileImportExport()
-APIRquest = MovieRequests()
 
-#Initialize app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout = html.Div(id='dash-app-id', children=[
-    dcc.Tabs([
-        dcc.Tab(label=tab_one_label, id='tab-one-id', children=[
-            html.Div(id='title-page-id', children=[
-                welcome_form,
-                instruction_capabilities_forms,
-                html.Div(id='import-movie-data-row-id', children=[
-                    confirm_movies_form
-                ])
-            ])  
-        ]),
-        dcc.Tab(label=tab_two_label, id='tab-two-id', children=[
-            instructions_pulling_movies_form,
-            configure_cluster_algorithm,
-            results_section
-        ])  
-    ])
-])
+# Initialize app
+class DashApp:
+    def __init__(self):
+        self.app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+        self.app.layout = html.Div(
+            id="dash-app-id",
+            children=[
+                dcc.Tabs(
+                    [
+                        dcc.Tab(
+                            label=tab_one_label,
+                            id="tab-one-id",
+                            children=[
+                                html.Div(
+                                    id="title-page-id",
+                                    children=[
+                                        welcome_form,
+                                        instruction_capabilities_forms,
+                                        html.Div(
+                                            id="import-movie-data-row-id",
+                                            children=[confirm_movies_form],
+                                        ),
+                                    ],
+                                )
+                            ],
+                        ),
+                        dcc.Tab(
+                            label=tab_two_label,
+                            id="tab-two-id",
+                            children=[
+                                instructions_pulling_movies_form,
+                                configure_cluster_algorithm,
+                                results_section,
+                            ],
+                        ),
+                    ]
+                )
+            ],
+        )
+        self.APIRequests = MovieRequests()
+
+        self.APIRquest.import_movie_data_app(app)
+
+    def run(self):
+        webbrowser.open_new("http://127.0.0.1:8050/")
+        self.app.run_server(debug=True, use_reloader=False)
 
 
-FileImportExport.app_upload_file(app)
-APIRquest.import_movie_data_app(app)
-
-def open_browser():
-    webbrowser.open_new('http://127.0.0.1:8050/')
-
-if __name__ == '__main__':
-    Timer(1, open_browser).start();
-    app.run_server(debug=True, use_reloader=False)
+if __name__ == "__main__":
+    app = DashApp()
+    app.run()
